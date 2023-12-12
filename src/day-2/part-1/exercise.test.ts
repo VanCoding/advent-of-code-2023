b9e128e2d6a5a0f1d7596b1b92b3f89f3777bcb7
Patrik Stutz <patrik.stutz@gmail.com>
2023-12-12 18:52:23 +0100

    day 2 part 1

---
+++ src/day-2/part-1/exercise.test.ts
@@ -0,0 +1,78 @@
+import { describe, expect, it } from "vitest";
+import { Game, gameIsPossible, parseGames, solve } from "./exercise";
+import {
+  EXAMPLE_DICE_COUNTS,
+  EXAMPLE_GAMES,
+  EXAMPLE_POSSIBILITIES,
+  EXAMPLE_SOLUTION,
+  EXERCISE_DICE_COUNTS,
+  EXERCISE_GAMES,
+  EXERCISE_SOLUTION,
+} from "./input";
+
+describe("parseGames", () => {
+  it("parses games correctly", () => {
+    expect(parseGames(EXAMPLE_GAMES)).toEqual([
+      {
+        id: 1,
+        sets: [
+          { blue: 3, red: 4, green: 0 },
+          { blue: 6, red: 1, green: 2 },
+          { blue: 0, red: 0, green: 2 },
+        ],
+      },
+      {
+        id: 2,
+        sets: [
+          { blue: 1, red: 0, green: 2 },
+          { blue: 4, red: 1, green: 3 },
+          { blue: 1, red: 0, green: 1 },
+        ],
+      },
+      {
+        id: 3,
+        sets: [
+          { blue: 6, red: 20, green: 8 },
+          { blue: 5, red: 4, green: 13 },
+          { blue: 0, red: 1, green: 5 },
+        ],
+      },
+      {
+        id: 4,
+        sets: [
+          { blue: 6, red: 3, green: 1 },
+          { blue: 0, red: 6, green: 3 },
+          { blue: 15, red: 14, green: 3 },
+        ],
+      },
+      {
+        id: 5,
+        sets: [
+          { blue: 1, red: 6, green: 3 },
+          { blue: 2, red: 1, green: 2 },
+        ],
+      },
+    ] satisfies Game[]);
+  });
+});
+
+describe("gameIsPossible", () => {
+  it("properly detects game possibility on example games", () => {
+    expect(
+      parseGames(EXAMPLE_GAMES).map((game) =>
+        gameIsPossible(game, EXAMPLE_DICE_COUNTS)
+      )
+    ).toEqual(EXAMPLE_POSSIBILITIES);
+  });
+});
+
+describe("solve", () => {
+  it("properly solves example", () => {
+    expect(solve(EXAMPLE_GAMES, EXAMPLE_DICE_COUNTS)).toEqual(EXAMPLE_SOLUTION);
+  });
+  it("properly solves exercise", () => {
+    expect(solve(EXERCISE_GAMES, EXERCISE_DICE_COUNTS)).toEqual(
+      EXERCISE_SOLUTION
+    );
+  });
+});
---
+++ src/day-2/part-1/exercise.ts
@@ -0,0 +1,75 @@
+import { filter, map, maxBy, pipe, sumBy } from "remeda";
+
+export type Game = {
+  id: number;
+  sets: DiceCounts[];
+};
+
+export type DiceCounts = {
+  red: number;
+  green: number;
+  blue: number;
+};
+
+type Color = "red" | "green" | "blue";
+
+type Part = {
+  count: number;
+  color: Color;
+};
+
+const findColorCount = (parts: Part[], color: Color) =>
+  parts.find((p) => p.color === color)?.count ?? 0;
+
+export const parseGames = (data: string): Game[] =>
+  map(data.split("\n"), (line) => {
+    const [header, sets] = line.split(": ");
+    const [game, id] = header.split(" ");
+    return {
+      id: +id,
+      sets: map(sets.split("; "), (set) => {
+        const parts = map(set.split(", "), (entry) => {
+          const [count, color] = entry.split(" ");
+          return { count: +count, color } as Part;
+        });
+        return {
+          red: findColorCount(parts, "red"),
+          blue: findColorCount(parts, "blue"),
+          green: findColorCount(parts, "green"),
+        };
+      }),
+    };
+  });
+
+const getMaxDiceCount = (game: Game, getCount: (set: DiceCounts) => number) =>
+  maxBy(game.sets.map(getCount), (v) => v) ?? 0;
+
+const colorIsPossible = (
+  game: Game,
+  maxCounts: DiceCounts,
+  diceCounts: DiceCounts,
+  getCount: (set: DiceCounts) => number
+) => getCount(maxCounts) <= getCount(diceCounts);
+const getMaxDiceCounts = (game: Game): DiceCounts => {
+  return {
+    blue: getMaxDiceCount(game, (v) => v.blue),
+    red: getMaxDiceCount(game, (v) => v.red),
+    green: getMaxDiceCount(game, (v) => v.green),
+  };
+};
+
+export const gameIsPossible = (game: Game, diceCounts: DiceCounts) => {
+  const maxCounts = getMaxDiceCounts(game);
+  return (
+    colorIsPossible(game, maxCounts, diceCounts, (v) => v.blue) &&
+    colorIsPossible(game, maxCounts, diceCounts, (v) => v.red) &&
+    colorIsPossible(game, maxCounts, diceCounts, (v) => v.green)
+  );
+};
+
+export const solve = (input: string, diceCounts: DiceCounts): number =>
+  pipe(
+    parseGames(input),
+    filter((game) => gameIsPossible(game, diceCounts)),
+    sumBy((game) => game.id)
+  );
